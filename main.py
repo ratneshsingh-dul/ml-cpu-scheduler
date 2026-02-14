f66acc8314f311b79ef8513dda57ed97196492f9
Ratnesh Singh <ratneshhsinghh005@gmail.com>
2026-02-14 09:10:42 +0530

    Added SJF scheduling and updated comparison in main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from scheduler import Process,fcfs
+from scheduler import Process,fcfs,sjf
 processes=[
     Process(1, 0, 5),
     Process(2, 1, 3),
     Process(3, 2, 8),
     Process(4, 3, 6)
 ]
-result=fcfs(processes)
-print("FCFS result :",result)
+
+print("FCFS result :",fcfs(processes))
+print("SJF result :",sjf(processes))
--- scheduler.py
+++ scheduler.py
@@ -23,3 +23,31 @@ def fcfs(processes):
     "average_wt":total_wt/n,
     "average_tat":total_tat/n
     }
+
+def sjf(processes):
+    process_copy=processes.copy()
+    current_time=0
+    total_wt=0
+    total_tat=0
+    completed=[]
+    n=len(processes)
+    while process_copy:
+        available=[]
+        for i in process_copy:
+            if i.arri_time<=current_time:
+                available.append(i)
+        if not available:
+            current_time=min(p.arri_time for p in process_copy)
+            continue
+        shortest=min(available,key=lambda x:x.bur_time)
+        wt=current_time-shortest.arri_time
+        tat=wt+shortest.bur_time
+        total_wt+=wt
+        total_tat+=tat
+        current_time+=shortest.bur_time
+        completed.append(shortest)
+        process_copy.remove(shortest)
+    return{
+    "average_wt":total_wt/n,
+    "average_tat":total_tat/n
+    }

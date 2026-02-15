6daa1a1b77dd64dce15c498dd19da9ae61f63990
Ratnesh Singh <ratneshhsinghh005@gmail.com>
2026-02-15 07:57:20 +0530

    Added round robin  scheduling and updated comparison in main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from scheduler import Process,fcfs,sjf
-processes=[
-    Process(1, 0, 5),
-    Process(2, 1, 3),
-    Process(3, 2, 8),
-    Process(4, 3, 6)
+from scheduler import Process,fcfs,sjf,rr
+processes = [
+    Process(1, 0, 5, priority=2),
+    Process(2, 1, 3, priority=1),
+    Process(3, 2, 8, priority=4),
+    Process(4, 3, 6, priority=3)
 ]
 
 print("FCFS result :",fcfs(processes))
 print("SJF result :",sjf(processes))
+print("Round Robin result :",rr(processes,5))
--- scheduler.py
+++ scheduler.py
@@ -39,7 +39,7 @@ def sjf(processes):
         if not available:
             current_time=min(p.arri_time for p in process_copy)
             continue
-        shortest=min(available,key=lambda x:x.bur_time)
+        shortest=min(available,key=lambda x:(x.bur_time,x.arri_time))
         wt=current_time-shortest.arri_time
         tat=wt+shortest.bur_time
         total_wt+=wt
@@ -47,6 +47,45 @@ def sjf(processes):
         current_time+=shortest.bur_time
         completed.append(shortest)
         process_copy.remove(shortest)
+    return{
+    "average_wt":total_wt/n,
+    "average_tat":total_tat/n
+    }
+
+from collections import deque
+def rr(processes,quantum=2):
+    processes=sorted(processes,key=lambda x:(x.arri_time,x.pid))
+    n=len(processes)
+    remaining_bt={p.pid:p.bur_time for p in processes}
+    queue=deque()
+    current_time=0
+    completion_time={}
+    i=0
+    while queue or i<n:
+        while i<n and processes[i].arri_time<=current_time:
+            queue.append(processes[i])
+            i+=1
+        if not queue:
+            current_time=processes[i].arri_time
+            continue
+        process=queue.popleft()
+        execution_time=min(quantum,remaining_bt[process.pid])
+        remaining_bt[process.pid]-=execution_time
+        current_time+=execution_time
+        while i<n and processes[i].arri_time<=current_time:
+            queue.append(processes[i])
+            i+=1
+        if remaining_bt[process.pid]>0:
+            queue.append(process)
+        if remaining_bt[process.pid]==0:
+            completion_time[process.pid]=current_time
+    total_tat=0
+    total_wt=0
+    for p in processes:
+        tat=completion_time[p.pid]-p.arri_time
+        wt=tat-p.bur_time
+        total_tat+=tat
+        total_wt+=wt
     return{
     "average_wt":total_wt/n,
     "average_tat":total_tat/n
